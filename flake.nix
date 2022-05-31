fef5c455a15808e41f740f9faddb856110bd07bc
Joshua Chapman <joshua.chapman@joshuachp.dev>
2022-05-31 21:24:52 +0200

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,66 @@
+{
+  inputs = {
+    nixpkgs = {
+      url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    };
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    naersk = {
+      url = "github:nmattia/naersk/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+  };
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    naersk,
+    fenix,
+    ...
+  }: let
+    eachDefaultSystemMap = flake-utils.lib.eachDefaultSystemMap;
+  in rec {
+    packages = eachDefaultSystemMap (system: let
+      naersk-lib = naersk.lib.${system};
+      fenix-pkg = fenix.packages.${system}.stable;
+    in {
+      default =
+        (naersk-lib.override {
+          inherit (fenix-pkg) cargo rustc;
+        })
+        .buildPackage {root = ./.;};
+    });
+    apps = eachDefaultSystemMap (system: {
+      default = flake-utils.lib.mkApp {
+        drv = packages.${system}.default;
+      };
+    });
+    devShells = eachDefaultSystemMap (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+      fenix-pkg = fenix.packages.${system}.stable;
+    in {
+      default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          (fenix-pkg.withComponents [
+            "cargo"
+            "clippy"
+            "rust-src"
+            "rustc"
+            "rustfmt"
+          ])
+          pre-commit
+        ];
+      };
+    });
+  };
+}
---
+++ src/lib.rs
@@ -0,0 +1,8 @@
+#[cfg(test)]
+mod test {
+
+    #[test]
+    fn test() {
+        assert_eq!(2 + 2, 4);
+    }
+}
