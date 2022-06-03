3e8c990ffadadbe3805a49853cfc2cdf4b19acd3
Joshua Chapman <joshua.chapman@joshuachp.dev>
2022-06-03 13:24:20 +0200

    feat: add examples

---
+++ examples/insertions.rs
@@ -0,0 +1,19 @@
+use forest::tree::Tree;
+
+const SIZE: i64 = 10000000;
+
+fn main() {
+    let mut tree: Tree<i64> = Tree::with_capacity(SIZE.try_into().unwrap());
+
+    (0..SIZE).for_each(|i| tree.append_child(i));
+
+    let mut sum = 0;
+    tree.iter().enumerate().for_each(|(i, v)| {
+        let expected: i64 = i.try_into().unwrap();
+        assert_eq!(expected, *v);
+
+        sum += *v;
+    });
+
+    println!("{}", sum);
+}
