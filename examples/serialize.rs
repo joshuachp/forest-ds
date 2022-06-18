ce8863ecc47b1194dde7c855d57a329237d545d7
Joshua Chapman <joshua.chapman@joshuachp.dev>
2022-06-18 19:18:21 +0200

    fix: renamed method in example

--- examples/serialize.rs
+++ examples/serialize.rs
@@ -5,16 +5,16 @@ fn main() {
 
     let root = tree.append_child(1);
 
-    let first = tree.insert_child(&root, 2).unwrap();
-    let second = tree.insert_sibling(&first, 3).unwrap();
+    let first = tree.append_child_to(&root, 2).unwrap();
+    let second = tree.insert_sibling_after(&first, 3).unwrap();
 
-    tree.insert_child(&first, 1);
-    tree.insert_child(&first, 2);
-    tree.insert_child(&first, 3);
+    tree.append_child_to(&first, 1);
+    tree.append_child_to(&first, 2);
+    tree.append_child_to(&first, 3);
 
-    tree.insert_child(&second, 1);
-    tree.insert_child(&second, 2);
-    tree.insert_child(&second, 3);
+    tree.append_child_to(&second, 1);
+    tree.append_child_to(&second, 2);
+    tree.append_child_to(&second, 3);
 
     let json = serde_json::to_string(&tree).unwrap();
 
