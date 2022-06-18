b5dc6d23d270572414bc9bf716659b64ad943798
Joshua Chapman <joshua.chapman@joshuachp.dev>
2022-06-18 19:19:20 +0200

    fix: use result in example

--- examples/serialize.rs
+++ examples/serialize.rs
@@ -8,13 +8,13 @@ fn main() {
     let first = tree.append_child_to(&root, 2).unwrap();
     let second = tree.insert_sibling_after(&first, 3).unwrap();
 
-    tree.append_child_to(&first, 1);
-    tree.append_child_to(&first, 2);
-    tree.append_child_to(&first, 3);
+    tree.append_child_to(&first, 1).unwrap();
+    tree.append_child_to(&first, 2).unwrap();
+    tree.append_child_to(&first, 3).unwrap();
 
-    tree.append_child_to(&second, 1);
-    tree.append_child_to(&second, 2);
-    tree.append_child_to(&second, 3);
+    tree.append_child_to(&second, 1).unwrap();
+    tree.append_child_to(&second, 2).unwrap();
+    tree.append_child_to(&second, 3).unwrap();
 
     let json = serde_json::to_string(&tree).unwrap();
 
